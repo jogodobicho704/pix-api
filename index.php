8f040b27c2648ca3a624e1149fe687d49eaa34f1
jogodobicho704 <jogodobicho704@gmail.com>
2026-01-30 09:10:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 header("Content-Type: application/json");
+http_response_code(200);
 
 echo json_encode([
   "status" => "online",
-  "time" => time()
+  "timestamp" => microtime(true)
 ]);
+
+flush();
