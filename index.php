738ef1909e62501c892815fa315a9c0b713841db
jogodobicho704 <jogodobicho704@gmail.com>
2026-01-30 09:00:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
-
 header("Content-Type: application/json");
 
-if (isset($_GET['test'])) {
-    echo json_encode([
-        "status" => "online",
-        "php" => phpversion()
-    ]);
-    exit;
-}
-
 echo json_encode([
-    "msg" => "endpoint ativo"
+  "status" => "online",
+  "time" => time()
 ]);
